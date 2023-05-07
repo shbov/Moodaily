15123117b19378217d291722d03e07ce0a98dad5
Andrew Shubnikov <a.shbov@yandex.ru>
2023-05-07 15:50:32 +0300

    add new emotions

--- src/Enums/Emotion.ts
+++ src/Enums/Emotion.ts
@@ -1,28 +1,54 @@
-// export enum Emotion {
-//   HAPPY = 'happy.png',
-//   JOYFUL = 'joyful.png',
-//   CHILL = 'chill.png',
-//   EXITED = 'exited.png',
-//   CONCERNED = 'concerned.png',
-//   SURPRISED = 'surprised.png',
-//   CONFUSED = 'confused.png',
-//   APATHETIC = 'apathetic.png',
-//   ANXIOUS = 'anxious',
-//   STRESS = 'stress',
-// }
-
-// export interface IEmotionRecord {
-//   [key: string]: EmotionType;
-// }
+import {ImageSourcePropType} from 'react-native';
 
 export const Emotion = {
   happy: {
     name: 'Счастливый',
     source: require('../../assets/emotions/happy.png'),
   },
+  joyful: {
+    name: 'Радостный',
+    source: require('../../assets/emotions/joyful.png'),
+  },
+
+  exited: {
+    name: 'Взволнованный',
+    source: require('../../assets/emotions/excited.png'),
+  },
+
+  chill: {
+    name: 'Расслабленный',
+    source: require('../../assets/emotions/chill.png'),
+  },
+
+  confused: {
+    name: 'Запутанный',
+    source: require('../../assets/emotions/confused.png'),
+  },
+  concerned: {
+    name: 'Обеспокоенный',
+    source: require('../../assets/emotions/concerned.png'),
+  },
+  apathetic: {
+    name: 'Апатичный',
+    source: require('../../assets/emotions/apathetic.png'),
+  },
+
+  angry: {
+    name: 'Злой',
+    source: require('../../assets/emotions/anger.png'),
+  },
+
+  anxious: {
+    name: 'Тревожный',
+    source: require('../../assets/emotions/anxious.png'),
+  },
 };
 
-// export type EmotionType = {
-//   source: ImageSourcePropType;
-//   name: string;
-// };
+export type EmotionType = {
+  name: string;
+  source: ImageSourcePropType;
+};
+//
+// export interface IEmotionRecord {
+//   [key: string]: EmotionType;
+// }
